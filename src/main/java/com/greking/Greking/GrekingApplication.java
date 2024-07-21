cdc28ce881a19858b9e9eca171601727d70d84a8
millook <kkmj1374@gmail.com>
2024-07-21 16:53:10 +0900

    feat: first commit

---
+++ src/main/java/com/greking/Greking/GrekingApplication.java
@@ -0,0 +1,13 @@
+package com.greking.Greking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrekingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrekingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greking/Greking/GrekingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greking.Greking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrekingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
